8f5ee2095fb7c439428a7a8a3b7f251d588f0f0f
chenyu <4325548@163.com>
2014-04-10 13:51:06 +0800

    add the ToolsObject static method

---
+++ OPENUI/OPENUI/ChatWithFace/ChatLabelCell.h
@@ -0,0 +1,13 @@
+//
+//  ChatLabelCell.h
+//  OPENUI
+//
+//  Created by chenyu on 14-4-10.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatLabelCell : UILabel
+
+@end
---
+++ OPENUI/OPENUI/ChatWithFace/ChatLabelCell.m
@@ -0,0 +1,31 @@
+//
+//  ChatLabelCell.m
+//  OPENUI
+//
+//  Created by chenyu on 14-4-10.
+//
+//
+
+#import "ChatLabelCell.h"
+
+@implementation ChatLabelCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
